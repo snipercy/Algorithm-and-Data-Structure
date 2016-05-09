b148ad5477be690afe56c249f2e08a5b10ce8809
531671430@qq.com <531671430@qq.com>
2016-05-09 22:04:52 +0800

    modified:   .gitignore 	modified:   java/src/com/company/HexToDec.java

--- java/src/com/company/HexToDec.java
+++ java/src/com/company/HexToDec.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 /**
  * 进制转换
@@ -10,7 +11,19 @@ public class HexToDec {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNextLine()) {
-
+            String str = sc.nextLine();
+            int len = str.length();
+            long res = 0;
+            int bit = 0;
+            for (int i = len - 1; i >= 2 ; i--) {
+                if (str.charAt(i) <= '9' && str.charAt(i) >= '0') {
+                    res += (str.charAt(i) - '0') * Math.pow(16, bit);
+                } else {
+                    res += (10 + str.charAt(i) - 'A') * Math.pow(16, bit);
+                }
+                bit++;
+            }
+            System.out.println(res);
         }
     }
 }
